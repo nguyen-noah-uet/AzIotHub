585fd78dd343e26fce06e9a8f917d1a5ac8446a8
Louis <Louis@DESKTOP-FROFOSB>
2023-03-31 20:20:00 +0700

    Add project files.

---
+++ AzIotHub.Cli/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+namespace AzIotHub.Cli;
+
+internal class Program
+{
+	private static DeviceClient _deviceClient;
+	private static readonly string 
+		DeviceConnectionString = "HostName=cosodoluong-hub.azure-devices.net;DeviceId=RaspberryPi;SharedAccessKey=gBmI4erLbh0ua9SkbKKMhZBLpyIoXddExhrcprhQp4c=";
+
+	private static async Task Main(string[] args)
+	{
+		_deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString, TransportType.Mqtt);
+		var telemetryDataPoint = new
+		{
+			time_stamp=DateTime.Now,
+			temperature = 25,
+			humidity = 70
+		};
+		string s = JsonConvert.SerializeObject(telemetryDataPoint);
+		Console.WriteLine(s);
+		Message message = new Message(Encoding.ASCII.GetBytes(s)) { ContentType = "application/json" };
+		try
+		{
+			await _deviceClient.SendEventAsync(message);
+			
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine("Exception while sending message: {0}", ex.Message);
+		}
+		
+	}
+}
